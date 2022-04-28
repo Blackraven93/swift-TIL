c0eded02980211566d75df97a12f030ba5a616d8
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-28 09:17:11 +0900

    Swift: 두개 뽑아서 더하기

--- algo.playground/Contents.swift
+++ algo.playground/Contents.swift
@@ -8,3 +8,46 @@ for _ in 0..<b {
     print(Array(repeating: "*", count: a).joined())
 }
 
+
+/**
+ 
+ */
+
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result = [Int]()
+
+    for i in 0 ..< numbers.count {
+        for j in i + 1 ..< numbers.count {
+            let num = numbers[i] + numbers[j]
+            if !result.contains(num) {
+                result.append(num)
+            }
+        }
+    }
+    return result.sorted()
+}
+
+
+import Foundation
+
+func mySolution(_ numbers:[Int]) -> [Int] {
+    var temp:[Int] = []
+
+    for x in 0...numbers.count - 2 {
+        for y in (x + 1)...numbers.count - 1 {
+
+            temp.append(numbers[x]+numbers[y])
+        }
+    }
+
+
+
+    let removedDuplicate: Set = Set(temp)
+
+    return Array(removedDuplicate).sorted(by: <)
+}
+
+
+print(solution([2,20,22,40,7,13]))
+print(mySolution([2,20,22,40,7,13]))
