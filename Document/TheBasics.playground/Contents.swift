43ad2c50ac4f3117c56854027338d5d7151a96ad
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-01 22:02:32 +0900

    Swift Docs: 기본 할당

---
+++ Document/TheBasics.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let maximumNumberofLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+print(x, y, z)
+
+// Type Annotations
+
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+
+print("The current value of friendlyWelcome is \(welcomeMessage)")
+
+
+/**
+ 코멘트
+ */
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
