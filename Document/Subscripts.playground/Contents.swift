d02a8dbc616f2b3f3b4325ec037ca02e0192eca7
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-19 00:51:59 +0900

    Swift Docs: Subscripts

---
+++ Document/Subscripts.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])") // 한번 사용할 수 있는건가..?
+// Prints "six times three is 18
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 3, columns: 3)
+
+print(matrix)
+matrix[0, 1] = 10
+matrix[1, 0] = 12
+print(matrix)
+
+let someValue = matrix[2,2]
+print(someValue) // swift에서 테스트 코드 사용하기!
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4]
+print(mars)
+
+print(mars)
