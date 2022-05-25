8cac1384b3d00414feede2a2453dd952a46fc7a3
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-26 03:14:22 +0900

    Swift Docs: Nested Types

---
+++ Document/NestedTypes.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+struct BlackjackCard {
+
+    // nested Suit enumeration
+    enum Suit: Character {
+        case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
+    }
+
+    // nested Rank enumeration
+    enum Rank: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    // func으로 설정하지 않고 메소드를 정의할 때는 이렇게?
+    var description: String { // 구조체 내에서 프로퍼티를 핸들링 할 때 이런 문법을 사용한다.
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
+
+// enum을 struct 안에 정의하고 파라미터로 받을 수 있다.
+
+let theAceOfSpades = BlackjackCard(rank: .ace, suit: .spades)
+print("theAceOfSpades: \(theAceOfSpades.description)")
+// Prints "theAceOfSpades: suit is ♠, value is 1 or 11
+
+let heartsSymbol = BlackjackCard.Suit.hearts.rawValue
+// heartsSymbol is "♡"
+print(heartsSymbol)
