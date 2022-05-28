c72a258bcd0a472d13c82e3b48db64d5a0c7e4d0
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-29 01:00:10 +0900

    Swift Docs: Protocols (2)

--- Document/Protocols.playground/Contents.swift
+++ Document/Protocols.playground/Contents.swift
@@ -112,87 +112,87 @@ class SomeSubClass: SomeSuperClass, SomeProtocol {
 
 // protocol은 대문자로 사용해야 한다
 
-class Dice {
-    let sides: Int
-    let generator: RandomNumberGenerator
-    init(sides: Int, generator: RandomNumberGenerator) {
-        self.sides = sides
-        self.generator = generator
-    }
-    func roll() -> Int {
-        return Int(generator.random() * Double(sides)) + 1
-    }
-}
+//class Dice {
+//    let sides: Int
+//    let generator: RandomNumberGenerator
+//    init(sides: Int, generator: RandomNumberGenerator) {
+//        self.sides = sides
+//        self.generator = generator
+//    }
+//    func roll() -> Int {
+//        return Int(generator.random() * Double(sides)) + 1
+//    }
+//}
+//
+//var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+//for _ in 1...5 {
+//    print("Random dice roll is \(d6.roll())")
+//}
+//
+//
+//protocol DiceGame {
+//    var dice: Dice { get }
+//    func play()
+//}
+//protocol DiceGameDelegate: AnyObject {
+//    func gameDidStart(_ game: DiceGame)
+//    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+//    func gameDidEnd(_ game: DiceGame)
+//}
 
-var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
-for _ in 1...5 {
-    print("Random dice roll is \(d6.roll())")
-}
-
-
-protocol DiceGame {
-    var dice: Dice { get }
-    func play()
-}
-protocol DiceGameDelegate: AnyObject {
-    func gameDidStart(_ game: DiceGame)
-    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
-    func gameDidEnd(_ game: DiceGame)
-}
-
-class SnakesAndLadders: DiceGame {
-    let finalSquare = 25
-    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
-    var square = 0
-    var board: [Int]
-    init() {
-        board = Array(repeating: 0, count: finalSquare + 1)
-        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
-        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
-    }
-    weak var delegate: DiceGameDelegate?
-    func play() {
-        square = 0
-        delegate?.gameDidStart(self)
-        gameLoop: while square != finalSquare {
-            let diceRoll = dice.roll()
-            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
-            switch square + diceRoll {
-            case finalSquare:
-                break gameLoop
-            case let newSquare where newSquare > finalSquare:
-                continue gameLoop
-            default:
-                square += diceRoll
-                square += board[square]
-            }
-        }
-        delegate?.gameDidEnd(self)
-    }
-}
-
-class DiceGameTracker: DiceGameDelegate {
-    var numberOfTurns = 0
-    func gameDidStart(_ game: DiceGame) {
-        numberOfTurns = 0
-        if game is SnakesAndLadders {
-            print("Started a new game of Snakes and Ladders")
-        }
-        print("The game is using a \(game.dice.sides)-sided dice")
-    }
-    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
-        numberOfTurns += 1
-        print("Rolled a \(diceRoll)")
-    }
-    func gameDidEnd(_ game: DiceGame) {
-        print("The game lasted for \(numberOfTurns) turns")
-    }
-}
-
-let tracker = DiceGameTracker()
-let game = SnakesAndLadders()
-game.delegate = tracker
-game.play()
+//class SnakesAndLadders: DiceGame {
+//    let finalSquare = 25
+//    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+//    var square = 0
+//    var board: [Int]
+//    init() {
+//        board = Array(repeating: 0, count: finalSquare + 1)
+//        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+//        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+//    }
+//    weak var delegate: DiceGameDelegate?
+//    func play() {
+//        square = 0
+//        delegate?.gameDidStart(self)
+//        gameLoop: while square != finalSquare {
+//            let diceRoll = dice.roll()
+//            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+//            switch square + diceRoll {
+//            case finalSquare:
+//                break gameLoop
+//            case let newSquare where newSquare > finalSquare:
+//                continue gameLoop
+//            default:
+//                square += diceRoll
+//                square += board[square]
+//            }
+//        }
+//        delegate?.gameDidEnd(self)
+//    }
+//}
+//
+//class DiceGameTracker: DiceGameDelegate {
+//    var numberOfTurns = 0
+//    func gameDidStart(_ game: DiceGame) {
+//        numberOfTurns = 0
+//        if game is SnakesAndLadders {
+//            print("Started a new game of Snakes and Ladders")
+//        }
+//        print("The game is using a \(game.dice.sides)-sided dice")
+//    }
+//    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+//        numberOfTurns += 1
+//        print("Rolled a \(diceRoll)")
+//    }
+//    func gameDidEnd(_ game: DiceGame) {
+//        print("The game lasted for \(numberOfTurns) turns")
+//    }
+//}
+//
+//let tracker = DiceGameTracker()
+//let game = SnakesAndLadders()
+//game.delegate = tracker
+//game.play()
 // Started a new game of Snakes and Ladders
 // The game is using a 6-sided dice
 // Rolled a 3
@@ -200,3 +200,171 @@ game.play()
 // Rolled a 4
 // Rolled a 5
 // The game lasted for 4 turns
+
+//extension Array: TextRepresentable where Element: TextRepresentable {
+//    var textualDescription: String {
+//        let itemsAsText = self.map { $0.textualDescription }
+//        return "[" + itemsAsText.joined(separator: ", ") + "]"
+//    }
+//}
+//let myDice = [d6, d12]
+//print(myDice.textualDescription)
+// Prints "[A 6-sided dice, A 12-sided dice]
+
+// protocol 중 정해져 있는 protocol
+// Hashable Equatable Comparable
+
+struct Vector3D: Equatable {
+    var x = 0.0, y = 0.0, z = 0.0
+}
+
+let twoThreeFour = Vector3D(x: 2.0, y: 3.0, z: 4.0)
+let anotherTwoThreeFour = Vector3D(x: 2.0, y: 3.0, z: 4.0)
+if twoThreeFour == anotherTwoThreeFour {
+    print("These two vectors are also equivalent.")
+}
+
+enum SkillLevel: Comparable {
+    case beginner
+    case intermediate
+    case expert(stars: Int)
+}
+
+// Comparable은 순서가 있음
+var levels = [SkillLevel.intermediate, SkillLevel.beginner,
+              SkillLevel.expert(stars: 5), SkillLevel.expert(stars: 3)]
+for level in levels.sorted() {
+    print(level)
+}
+
+// 상속 protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
+    // protocol definition goes here
+// }
+
+protocol Named {
+    var name: String { get }
+}
+protocol Aged {
+    var age: Int { get }
+}
+struct Person2: Named, Aged {
+    var name: String
+    var age: Int
+}
+// protocol composition 구성?
+func wishHappyBirthday(to celebrator: Named & Aged) {
+    print("Happy birthday, \(celebrator.name), you're \(celebrator.age)!")
+}
+let birthdayPerson = Person2(name: "Malcolm", age: 21)
+wishHappyBirthday(to: birthdayPerson)
+
+class Location {
+    var latitude: Double
+    var longitude: Double
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+class City: Location, Named {
+    var name: String
+    init(name: String, latitude: Double, longitude: Double) {
+        self.name = name
+        super.init(latitude: latitude, longitude: longitude)
+    }
+}
+func beginConcert(in location: Location & Named) {
+    print("Hello, \(location.name)!")
+}
+
+let seattle = City(name: "Seattle", latitude: 47.6, longitude: -122.3)
+beginConcert(in: seattle)
+// Prints "Hello, Seattle!
+
+// class나 struct와 함께 protocol을 받을 수 있다.
+
+protocol HasArea {
+    var area: Double { get }
+}
+
+class Circle: HasArea {
+    let pi = 3.1415927
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
+
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
+
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+
+// Optional Protocol Requirements
+@objc protocol CounterDataSource {
+    @objc optional func increment(forCount count: Int) -> Int
+    @objc optional var fixedIncrement: Int { get }
+}
+
+
+class Counter {
+    var count = 0
+    var dataSource: CounterDataSource?
+    func increment() {
+        if let amount = dataSource?.increment?(forCount: count) {
+            count += amount
+        } else if let amount = dataSource?.fixedIncrement {
+            count += amount
+        }
+    }
+}
+
+
+class ThreeSource: NSObject, CounterDataSource {
+    let fixedIncrement = 3
+}
+
+var counter = Counter()
+counter.dataSource = ThreeSource()
+for _ in 1...4 {
+    counter.increment()
+    print(counter.count)
+}
+
+class TowardsZeroSource: NSObject, CounterDataSource {
+    func increment(forCount count: Int) -> Int {
+        if count == 0 {
+            return 0
+        } else if count < 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+}
+
+counter.count = -4
+counter.dataSource = TowardsZeroSource()
+for _ in 1...5 {
+    counter.increment()
+    print(counter.count)
+}
+
+// 공부해야할 키워드 where, extension
