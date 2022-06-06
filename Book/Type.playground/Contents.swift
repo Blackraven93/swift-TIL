d668afee011a890d471a9ecd7ad4c919a919bb9b
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-06 22:35:10 +0900

    Swift: String type basic

--- Book/Type.playground/Contents.swift
+++ Book/Type.playground/Contents.swift
@@ -23,3 +23,34 @@ print(UInt.random(in: 0...30))
 print(Double.random(in: 1.5...4.5))
 print(Float.random(in: 1.5...4.5))
 
+// String
+
+// + 이나 append를 이용해 문자열 합성 가능
+var name:String = "My name is"
+name += " black"
+name.append(" raven")
+print(name)
+
+// count
+print(name.count)
+// empty
+print(name.isEmpty)
+
+var isSameString: Bool = true
+isSameString = name == "compare"
+print(isSameString)
+
+// 접두어, 접미어 확인
+print(name.hasPrefix("My name"))
+print(name.hasSuffix("raven"))
+
+print(name.uppercased())
+print(name.lowercased())
+
+// 긴문장
+let favoriteFood = """
+내가 좋아하는 음식은
+치킨입니다.
+"""
+
+print(favoriteFood)
