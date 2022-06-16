2d8e3f40fd4018958f0b66d7229942ed9ef2c603
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-16 23:41:35 +0900

    Swift: Operator 1

---
+++ Book/Operator.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var valueA: Int = 3
+var valueB: Int = 5
+
+var biggerValue: Int = valueA > valueB ? valueA : valueB
+print(biggerValue)
+
+valueA = 0
+valueB = -3
+
+biggerValue = valueA > valueB ? valueA : valueB
+
+var stringA: String = ""
+var stringB: String = "String"
+
+var resultValue: Double = stringA.isEmpty ? 1.0 : 0.0
+resultValue = stringB.isEmpty ? 1.0 : 0.0
+
+print(resultValue)
+
+var unsignedInteger: UInt8 = 0
+let errorUnderflowResult: UInt8 = unsignedInteger - 1
+let underflowedValue: UInt8 = unsignedInteger &- 1
+
+unsignedInteger = UInt8.max
+let errorOverflowResult: UInt8 = unsignedInteger + 1
+let overflowedValue: UInt8 = unsignedInteger &+ 1
+
